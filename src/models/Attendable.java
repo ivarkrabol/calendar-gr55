d2b41b68e77eef7ca9254948074b9daac96a74d5
Ivar H. Krabol <ivarhk@gmail.com>
2015-02-20 13:48:35 +0100

    began work on attendable class and database interaction

--- src/models/Attendable.java
+++ src/models/Attendable.java
@@ -1,5 +1,15 @@
 package models;
 
-public abstract class Attendable {
-	
+import java.util.List;
+
+public abstract class Attendable extends Model {
+
+    private User administrator;
+    private List<User> pending;
+    private List<User> attending;
+
+    public void attend(User user) {
+        pending.remove(user);
+        attending.remove(user);
+    }
 }
