b01fda76f488c9733d79244e1aa4e8411e61eefc
kesma <karielines@gmail.com>
2015-02-20 13:58:06 +0100

    added some fields to calendar class

--- src/models/Calendar.java
+++ src/models/Calendar.java
@@ -1,5 +1,27 @@
 package models;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Appointment;
+
 
 public class Calendar extends Model {
+	
+	private List<Appointment> calendar;
+	
+	public Calendar() {
+		this.calendar = new ArrayList<Appointment>();
+	}
+	
+	
+	public void addAppointment(Appointment appointment){
+		this.calendar.add(appointment);
+	}
 
+	
+	public void removeAppointment(Appointment appointment){
+		if(this.calendar.remove(appointment));
+	}
+	
+	
 }
