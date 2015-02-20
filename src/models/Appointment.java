6e01dc72fe458e33eb2b0cff8ad7dc28ab71ad1b
kesma <karielines@gmail.com>
2015-02-20 13:30:17 +0100

    added fields, getters and setters to Appointment class

--- src/models/Appointment.java
+++ src/models/Appointment.java
@@ -3,6 +3,10 @@ package models;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+
+//Appointment class
+
+
 public class Appointment extends Attendable{
 	
 	private String title;
@@ -10,19 +14,55 @@ public class Appointment extends Attendable{
 	private LocalDate date;
 	private LocalTime startTime;
 	private LocalTime endTime;
+	private Room room;
 	
 	
-
 	
+	public String getTitle() {
+		return title;
+	}
 	
-//	+ title: String
-//	+ description: String
-//	+ date: Date
-//	+ startTime: Time
-//	+ endTime: Time
-//	+ repeat: int
-//	+ endDate: Date
-//	+ room: Room
-//	+ User: Administrator
-
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public LocalDate getDate() {
+		return date;
+	}
+	
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	
+	public LocalTime getStartTime() {
+		return startTime;
+	}
+	
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime;
+	}
+	
+	public LocalTime getEndTime() {
+		return endTime;
+	}
+	
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime;
+	}
+	
+	public Room getRoom() {
+		return room;
+	}
+	
+	public void setRoom(Room room) {
+		this.room = room;
+	}
 }
