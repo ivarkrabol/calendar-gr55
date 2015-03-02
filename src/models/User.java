ede29e8c042566e68c84309ac69e2e5216d89c51
Ivar H. Krabol <ivarhk@gmail.com>
2015-03-02 11:13:17 +0100

    Created basic User model

--- src/models/User.java
+++ src/models/User.java
@@ -1,5 +1,34 @@
 package models;
 
+import exceptions.DBConnectionException;
+import util.DB;
+import util.ModelCache;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class User extends Model{
 
+    private String email;
+    private String lastName;
+    private String firstName;
+    private String phoneNr;
+
+    public static User getById(int id, DB db, ModelCache mc) throws SQLException, DBConnectionException {
+        User user;
+        if(mc.contains(User.class, id)) user = mc.get(User.class, id);
+        else user = new User();
+        ResultSet results = db.query("" +
+                "SELECT EMail, LastName, FirstName, PhoneNr\n" +
+                "FROM USER\n" +
+                "WHERE UserId = " + id);
+        if(!results.next()) throw new SQLException("No User with that id in database");
+        user.email = results.getString("EMail");
+        user.lastName = results.getString("LastName");
+        user.firstName = results.getString("FirstName");
+        user.phoneNr = results.getString("PhoneNr");
+        if(results.next()) throw new SQLException("Result not unique");
+        mc.put(id, user);
+        return user;
+    }
 }
