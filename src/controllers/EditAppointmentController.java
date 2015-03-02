ac74b3ea1b16a27376feb07f6068871c74e788fa
kesma <karielines@gmail.com>
2015-03-02 14:44:07 +0100

    added beans property to appointment

--- src/controllers/EditAppointmentController.java
+++ src/controllers/EditAppointmentController.java
@@ -42,9 +42,7 @@ public class EditAppointmentController extends Controller{
             appointmentModel.setDescription(descriptionField.getText());
             appointmentModel.setStartTime(this.startTime);
             appointmentModel.setEndTime(this.endTime);
-            int generatedID = appointmentModel.generateID();
-            appointmentModel.setID(generatedID);
-            appointmentModel.setDate(this.date);
+
             //TODO: add the appointment to the users calendar
             this.getStage().close();
         }
