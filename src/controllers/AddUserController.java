8c683f9dde65b1aad89c6f6d4b4a0e5b9e7b6023
jmfilberg <jmfilberg@gmail.com>
2015-03-06 14:24:48 +0100

    laget addUser view og kontroller

---
+++ src/controllers/AddUserController.java
@@ -0,0 +1,27 @@
+package controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AddUserController {
+	@FXML
+    private TextField fname;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private TextField lname;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField phone;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private TextField email;
+}
